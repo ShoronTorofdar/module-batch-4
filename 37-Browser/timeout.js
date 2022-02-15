26e5ee6424d1f939838dcb92e24cd4da82bf6d20
ShoronTorofdar <86644344+ShoronTorofdar@users.noreply.github.com>
2022-02-15 11:30:10 +0600

    browser debuging start

---
+++ 37-Browser/timeout.js
@@ -0,0 +1,3 @@
+console.log('First: i will be a firstclass slave of Allah');
+console.log('Second: i will be not  a secondclass slave of Allah');
+console.log('Third: i will be not  a Thirdclass slave of Allah');
